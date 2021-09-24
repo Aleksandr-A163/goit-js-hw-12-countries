a708495313b9ad0269c7f4274be111e098acd7ce
Aleksandr-A163 <quark_31@mail.ru>
2021-09-24 19:15:15 +0400

    вывел основную информацию

--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
 import './sass/main.scss';
+import fetchCountries from "./js/fetchCountries";
+import countryInfo from './templates/countryInfo.hbs';
+
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import debounce from 'lodash.debounce';
+import { alert } from '@pnotify/core/dist/PNotify.js';
+
+const textManyCountries = 'Too many matches found. Please enter a more specific query!';
+const textNoFound = 'Сheck the correctness of entered query';
+const refsMarkUP={
+    input: document.querySelector('.country-input'),
+    countriesMarkUp: document.querySelector('.countries-list'),
+    clearButton: document.querySelector('.country-clear-button'),
+    countryContainer: document.querySelector('.country-container'),  
+};
+
+
+refsMarkUP.input.addEventListener('input', debounce(onInput, 600))
+
+function onInput(e) {
+    const searchQuery= e.target.value;
+    fetchCountries(searchQuery)
+    .then((countries)=> {
+        console.log(countries);
+        if (countries.status === 404){
+          alertCountry(textNoFound, 'error');
+            return;
+        }
+        if(countries.length < 10){
+          renderCountries(countries);
+        }  else  {
+          alertCountry(textManyCountries,'notice'); 
+        }  
+    })
+    .catch((error)=> console.log("Описание ошибки", error));     
+};
+
+function renderCountries(countries){
+    if(countries.length ===1){
+ const list= countryInfo(...countries);
+ refsMarkUP.countriesMarkUp.innerHTML =list;
+    } else {
+      const list= countryInfo(countries);
+      refsMarkUP.countriesMarkUp.innerHTML =list;
+  };
+};
+
+function alertCountry(text, type) {
+  refsMarkUP.countriesMarkUp.innerHTML='';
+alert({
+  text,
+  type,
+});
+};
+
+refsMarkUP.clearButton.addEventListener('click', () => {
+  refsMarkUP.countryContainer.innerHTML = '';
+  refsMarkUP.countriesMarkUp.innerHTML = '';
+  refsMarkUP.input.value = '';
+});
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://restcountries.com/v2/'
+export default function fetchCountries(searchQuery) {
+
+  return  fetch(`${BASE_URL}name/${searchQuery}`)
+    .then(response=>response.json().catch(err => {
+
+    } ))   
+};
